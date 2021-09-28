bcbf5279aea2ac3c0414875a6d275efc92caacaa
kingForks <muhabdi47.com@gmail.com>
2021-09-29 03:21:02 +0800

    Create a route for ajax a stack-trace

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use CloudMyn\Logger\Facade\Logger;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/show/{filename?}', '\CloudMyn\Logger\Http\Controllers\LoggerController@show')->name('logger.show');
+
+Route::get('/ajax/trace/{filename}/{id}', '\CloudMyn\Logger\Http\Controllers\LoggerController@ajaxTrace')->name('ajax.trace');
 
 Route::get('/{filename}/delete', '\CloudMyn\Logger\Http\Controllers\LoggerController@delete')->name('logger.delete');
