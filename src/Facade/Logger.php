e24e6b1709f70b1d8f36d79430833df6972173a3
kingForks <muhabdi47.com@gmail.com>
2021-09-29 01:48:58 +0800

    Update facade method

--- src/Facade/Logger.php
+++ src/Facade/Logger.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Facade;
  *  @method static void log(\Throwable $throwable, ?\Illuminate\Database\Eloquent\Model $user)
  *  @method static array get(string $file_name) Method for get the error logs
  *  @method static array<string>|array<null> getLogFiles() Method for get log files
+ *  @method static array whereId(string $file_name, string $value) Method for find a log data, base on its id
  *  @method static array whereClass(string $file_name, string $value) Method for find a log data, base on its class
  *  @method static array whereMessage(string $file_name, string $value) Method for find a log data, base on its message
  *  @method static array whereIp(string $file_name, string $value) Method for find a log data, base on its Ip
