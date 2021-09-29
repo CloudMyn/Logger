2aacc812c7eeed620918127e07f53f610bf37936
kingForks <muhabdi47.com@gmail.com>
2021-09-29 14:42:31 +0800

    Update config/logger.php

--- config/logger.php
+++ config/logger.php
@@ -4,11 +4,6 @@
 return [
 
     /**
-     *  Define maximum size of file could be read in bytes
-     */
-    'chunks'    =>  500,
-
-    /**
      *  Define the previx of the route
      */
     'previx'    =>  'logger',
